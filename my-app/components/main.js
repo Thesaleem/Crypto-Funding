35e618e87348bda30ccc1d11e31c04e692efde88
Saleem <saleemismail14@yahoo.com>
2023-05-30 10:01:08 +0100

    update

--- my-app/components/main.js
+++ my-app/components/main.js
@@ -1,17 +1,18 @@
 import styles from '@/styles/Home.module.css'
+import Image from 'next/image'
 
 export default function Main ({handleClick}) {
     return (
         <div className={styles.hero}>
             <div className={styles.heroContent}>
             <h2 className={styles.heroHeader}>
-                Let's make the impossible, possible
+               {` Let's make the impossible, possible`}
             </h2>
             <p>Start a campaign and crowd fund goerli ether.</p>
             <button className={styles.button} onClick={() => handleClick(true)}>Start campaign</button>
             </div>
             <div className={styles.image}>
-            <img src="../pic.png" alt=""  />
+            <Image src="../pic.png" alt=""  />
             </div>
       </div>
     )
